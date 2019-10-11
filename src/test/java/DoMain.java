4045199618e5d30e2efeeb755ebf2f217d32947f
your name <499033522@qq.com>
2019-10-11 11:20:59 +0800

    添加

---
+++ src/test/java/DoMain.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+/**
+ * Create Date:2019年10月10日
+ */
+
+/**
+ * <br>Title:TODO 类标题
+ * <br>Description:TODO 类功能描述
+ * <br>Author:姚英杰
+ * <br>Date:2019年10月10日
+ */
+public class DoMain {
+  @Test
+  public void Test01() {
+    System.out.println("hello wored");
+  }
+
+  @Test
+  public void Test02() {
+    System.out.println("hello wored  next");
+  }
+}
